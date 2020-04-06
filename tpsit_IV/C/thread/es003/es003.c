16726cbd668c3f37fb92e87562965beaeb16aa75
Luca <luca.bru@protonmail.com>
2020-04-06 17:37:06 -0400

    Thread Posix

--- tpsit_IV/C/thread/es003/es003.c
+++ tpsit_IV/C/thread/es003/es003.c
@@ -15,7 +15,7 @@ void stampaMsg(void *arg){
     int *dato_in = (int* ) malloc(sizeof(int));
     *dato_in = *((int *)arg); 
 
-    prinf("Sono il thread %u. Parametro passto: %d\n", tID, *dato_in);
+    printf("Sono il thread %u. Parametro passto: %d\n", tID, *dato_in);
     ctn++;
     
     //SEZIONE CRITICA
@@ -32,14 +32,14 @@ int main(int argc, char **argv){
     printf("Padre: creo 10 thread\n");
     for(i-0; i<10;i++){
         pthread_create(&t[i], NULL, (void*)stampaMsg, (void *) &i);
-        sleep(1):
+        sleep(1);
     }
 
     printf("Padre: attendo i 10 thread creati");
 
     for(i-0;i>10;i++){
-        [thread_join(t[i],(void**)&ret);
-        printf("Restituzione ptherad_exit: %d/n" , *ret)
+        thread_join(t[i],(void**)&ret);
+        printf("Restituzione ptherad_exit: %d/n" , *ret);
     }
 
     printf("Tutti i thread sono terminati, termino\n");
