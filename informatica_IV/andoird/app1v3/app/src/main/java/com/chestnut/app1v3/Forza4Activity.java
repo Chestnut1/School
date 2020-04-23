2e84989cbce8214ed2a2fa18fb510149939c4711
Luca Bruno <luca.bru@protonmail.com>
2020-04-23 09:22:13 +0200

    .

--- informatica_IV/andoird/app1v3/app/src/main/java/com/chestnut/app1v3/Forza4Activity.java
+++ informatica_IV/andoird/app1v3/app/src/main/java/com/chestnut/app1v3/Forza4Activity.java
@@ -5,19 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
 import android.widget.TableRow;
 
 public class Forza4Activity extends AppCompatActivity {
     private static final String TAG = "Forza 4 acrtivity";
 
-    public int m[][];   //integer matrix
-    Button b[][];   //button matrix
-    public boolean player1 = true;
+    private final int ROWS = 6;
+    private final int COLS = 7;
+    private boolean player = true;  //true -> 1, false -> 2
 
-    TableRow tb[];
+    int m[][];
+    Button btn[][];
 
-    Button btn;
+
+    TableRow tb0, tb1, tb2, tb3, tb4, tb5;
 
 
     @Override
@@ -28,45 +33,86 @@ public class Forza4Activity extends AppCompatActivity {
         Log.e(TAG, "Activity avviata con successo");
 
         //initialize matrix
-        m = new int[6][7];
-        for(int r=0; r<6; r++){
-            for(int c=0; c<7; c++){
-                m[r][c] = 0;
+        m = new int[ROWS][COLS];
+        for(int i=0; i<ROWS; i++){
+            for(int j=0; j<COLS; j++){
+                m[i][j] = 0;
             }
         }
 
-        //binding
-        tb[0] = findViewById(R.id.tbR1);
-        tb[1] = findViewById(R.id.tbR2);
-        tb[2] = findViewById(R.id.tbR3);
-        tb[3] = findViewById(R.id.tbR4);
-        tb[4] = findViewById(R.id.tbR5);
-        tb[5] = findViewById(R.id.tbR6);
+        //bind components
+        bindCOmponents();
 
+    }
 
-        for(int i=0; i<6; i++){
-            for(int k=0; k<7; k++){
-                btn = new Button(this);
-                btn.setTransitionName("btn_"+ k +"_"+i);
-                btn.setBackgroundResource(R.color.azzurroTrasparente);
-                btn.setOnClickListener(new MyListener());
-                tb[k].addView(btn);
-            }
+    void bindCOmponents(){
+        btn = new Button[ROWS][COLS];
+
+        tb0 = findViewById(R.id.tbR0);
+        tb1 = findViewById(R.id.tbR1);
+        tb2 = findViewById(R.id.tbR2);
+        tb3 = findViewById(R.id.tbR3);
+        tb4 = findViewById(R.id.tbR4);
+        tb5 = findViewById(R.id.tbR5);
+
+        //create button matrix
+        Button btn;
+
+        for(int i=0; i<COLS; i++){
+            btn = new Button(this);
+            btn.setLayoutParams(new ViewGroup.LayoutParams(30, 30));
+            btn.setTransitionName("btn_0_"+i);
+            btn.setOnClickListener(new myListener());
+            tb0.addView(btn);
         }
-        //adding listeners
+        for(int i=0; i<COLS; i++){
+            btn = new Button(this);
+            btn.setLayoutParams(new ViewGroup.LayoutParams(30, 30));
+            btn.setTransitionName("btn_1_"+i);
+            btn.setOnClickListener(new myListener());
+            tb1.addView(btn);
+        }
+        for(int i=0; i<COLS; i++){
+            btn = new Button(this);
+            btn.setLayoutParams(new ViewGroup.LayoutParams(30, 30));
+            btn.setTransitionName("btn_2_"+i);
+            btn.setOnClickListener(new myListener());
+            tb2.addView(btn);
+        }
+        for(int i=0; i<COLS; i++){
+            btn = new Button(this);
+            btn.setLayoutParams(new ViewGroup.LayoutParams(30, 30));
+            btn.setTransitionName("btn_3_"+i);
+            btn.setOnClickListener(new myListener());
+            tb3.addView(btn);
+        }
+        for(int i=0; i<COLS; i++){
+            btn = new Button(this);
+            btn.setLayoutParams(new ViewGroup.LayoutParams(30, 30));
+            btn.setTransitionName("btn_4_"+i);
+            btn.setOnClickListener(new myListener());
+            tb4.addView(btn);
+        }
+        for(int i=0; i<COLS; i++){
+            btn = new Button(this);
+            btn.setLayoutParams(new ViewGroup.LayoutParams(50, 30));
+            btn.setTransitionName("btn_5_"+i);
+            btn.setOnClickListener(new myListener());
+            tb5.addView(btn);
+        }
     }
 
-    void bindComponents(){
+    class myListener implements View.OnClickListener{
+        private static final String TAG = "ClassListener";
 
-    }
-
-    class MyListener implements View.OnClickListener{
         @Override
         public void onClick(View v) {
+            Button b = (Button) v;
             int x,y;
-            Button btn = (Button) v;
-            x = Integer.parseInt(btn.getTransitionName().split("_")[1]);
-            y = Integer.parseInt(btn.getTransitionName().split("_")[1]);
+            x = Integer.parseInt(b.getTransitionName().split("_")[1]);
+            y = Integer.parseInt(b.getTransitionName().split("_")[2]);
+
+            inserisciMoneta(x,y);
         }
 
         private void inserisciMoneta(int x, int y){
