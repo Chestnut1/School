bdab62b79540caccbba6c245dd4e17fde6222f3b
Luca <luca.bru@protonmail.com>
2020-04-27 19:18:27 +0200

    Thread Posix

--- tpsit_IV/C/thread/es008_filosofi/es08.c
+++ tpsit_IV/C/thread/es008_filosofi/es08.c
@@ -11,77 +11,86 @@
 #define DESTRA (i+1)%N      //con queste due fromule ho sempre a disposizione il filosof a destra e a sinistra
 #define SINISTRA (i-1+N)%N
 
-pthread_mutex_t mutex, mutex_f[N];  //per accesso alle sezioni critiche (le forchette sul tavolo)
-int stato[N];   //variabile condivisa, contiene stato filosofi
+pthread_mutex_t mutex, mutex_f[N];  //mutex per risorse critiche, mutex_f per le forchette
+int stato[N];   //stato di ogni filosofo
 
-void pensa(int i){
-    printf("\nFILOSOFO %d sto pensando...\n", i);
-    sleep(rand()%N);    //randomizzo tempo per pensare
+void *pensa(int i){
+    printf("\nFILOSOFO %d, sto pensando..." , i);
+    sleep(rand()%N);
 }
 
-void mangia(int i){
-    printf("\nFILOSOFO %d sto mangiando...\n", i);
-    sleep(rand()%N);    //randomizzo tempo per mangiare
+void *mangia(int i){
+    printf("\nFILOSOFO %d, sto mangiando..." , i);
+    sleep(rand()%N);
 }
 
 void controllaPosate(int i){
     if(stato[i] == ATTESA && stato[DESTRA] != MANGIA && stato[SINISTRA] != MANGIA){
         stato[i] = MANGIA;
-        printf("\nFILOSOFO %d: prendo le forchette e mangio" , i);
+        printf("\nFILOSOFO %d, prendo le posate e mangio..." , i);
     }else{
-        printf("\nFILOSOFO %d: forchette occupate, attendo per mangiare" , i);
+        printf("\nFILOSOFO %d, forchette occupate, atteno per mangiare..." , i);
     }
 }
 
-void prendi(int i){
+void *prendi(int i){
     pthread_mutex_lock(&mutex); //accesso in mutua esclusione
-    printf("\nFILOSOFO %d: ho fame e aspetto le forchette...\n", i);
+    printf("\nFILOSOFO %d, ho fame e aspetto le forchette...", i);
+
     stato[i] = ATTESA;
 
-    controllaPosate(i); //controllo e attendo posata
-    
+    controllaPosate(i); //controlliamo e attendimao le posate
+
+    pthread_mutex_lock(&mutex_f[i]);    //blocca la sua risorsa
     pthread_mutex_unlock(&mutex);
 }
 
-void posa(int i){
-    pthread_mutex_lock(&mutex); //accesso in muta esclusione
-    stato[i] = PENSA;   //modifico lo stato (prevengo starvation e dead lock)
-    pthread_mutex_unlock(&mutex);   //rilasio la sezione critica
+void *posa(int i){
+    pthread_mutex_lock(&mutex);
+    stato[i] = PENSA;   //aggiorno lo stato
+    pthread_mutex_unlock(&mutex_f[i]);  //libera la sua risorsa
+    pthread_mutex_unlock(&mutex);
 }
 
+
 void *filosofo(void *arg){
-    int i = (*(int*)arg);   //casting
+    int i = (*(int*)arg);
+
     while(1){
-        //pensano
+        //pensa
         pensa(i);
-        //prendere le posate
+        //prende
         prendi(i);
-        //se le prendono mangiano
+        //mangia
         mangia(i);
-        //posano forchetta
+        //posa 
         posa(i);
     }
+
 }
 
 int main(int argc, char **argv){
-    int i;  //indice
+    int i = 0;  //indice
     pthread_t filo[N];  //filosofi
 
-    //inizialissi il semafoto di accesso
+    //inizializzo il semaforo ad acceso
     pthread_mutex_init(&mutex, NULL);
 
-    //inizializzo mutex f
-    for(int i=0; i<N; i++){
+    //inizializzo le mutex dei filosofi
+    for(i=0; i<N; i++){
         pthread_mutex_init(&mutex_f[i], NULL);
     }
 
     //creo e lancio i miei thread (filosofi)
-    for(int i=0; i<N; i++){
-        pthread_create(&filo[i], NULL, (void *)filosofo, (void*)&i);
+    for(i = 0; i < N; i ++){
+        pthread_create(&filo[i], NULL, (void *) filosofo, (void *)&i);
+        sleep(1);
     }
+    pthread_exit(0);
 
     return 0;
 }
+
 
 
 
