1e5274e28bb8d825be51e3a9c7f1140ce00bb30b
Luca Bruno <luca.bru@protonmail.com>
2020-05-10 23:17:18 +0200

    javascipt stuff

---
+++ informatica_IV/javascript/es002_form/script.js
@@ -0,0 +1,11 @@
+document.getElementById("myCheck").style.display = "none";
+
+function showSposata(){
+    document.getElementById("myCheck").style.display = "block";
+    td = document.getElementById("trSposata")
+
+    sposataList = document.createElement("input")
+    sposataList.
+    td.appendChild(sposataList)
+
+}
