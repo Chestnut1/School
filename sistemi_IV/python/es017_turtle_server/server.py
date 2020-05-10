d01b4ad07ac62786f70cd25368340a0666f0fc94
Luca Bruno <luca.bru@protonmail.com>
2020-05-10 22:46:05 +0200

    Turtle Multiserver | UDP

--- sistemi_IV/python/es017_turtle_server/server.py
+++ sistemi_IV/python/es017_turtle_server/server.py
@@ -1,31 +1,58 @@
 import socket
 import turtle
+import random
+
+server_ip = '127.0.0.1'
+server_port = 10000
+
+main_turtle = turtle.Turtle()
+main_turtle.hideturtle()
+
+turtle_table = {}
+port_table = []
+
+def move(c, p):
+    print(f"moving {p}")
+    if c == "w":
+        turtle_table[p].forward(10)
+    elif c == "a":
+        turtle_table[p].left(90)
+    elif c == "s":
+        turtle_table[p].backward(10)
+    elif c == "d":
+        turtle_table[p].right(90)
+
+
 
 def main():
-    turtle_main = turtle.Turtle()
-    #turtle_main.forward(1000)
-
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    server.bind(("127.0.0.1",5000))
-
-    address_table = {}
+    server.bind((server_ip, server_port))
 
     
+
     while True:
-        """
-        directon, address = server.recvfrom(4096)
+        command, address = server.recvfrom(4096)
+        command = command.decode()[0:]
+
         print(address)
+        print(port_table)
+        print(turtle_table)
 
-        found = False
-        for k,_ in address_table.items():
-            if k == address[1]:
-                found = True
+        if address[1] in port_table:
+            if command == "esc":
+                turtle_table.pop(address[1])
+                port_table.pop(port_table.index(address[1]))
+            else:
+                move(command,address[1])
+        else:
+            port_table.append(address[1])
+            turtle_table[address[1]] = main_turtle.clone()
+            turtle_table[address[1]].showturtle()
 
-        """
 
 
 
+    
 
 if __name__ == "__main__":
     main()
