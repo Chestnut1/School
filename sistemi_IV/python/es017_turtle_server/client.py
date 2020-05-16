3eaa906b87bd49523ed23dcb5d477bf6d4aae8e7
Luca Bruno <luca.bru@protonmail.com>
2020-05-16 08:37:03 +0200

    Some comments :+1:

--- sistemi_IV/python/es017_turtle_server/client.py
+++ sistemi_IV/python/es017_turtle_server/client.py
@@ -1,3 +1,15 @@
+"""
+Author: Bruno Luca
+Date: 16-05-2020
+This program create a UDP socket process that send to the serevr some commands based on key pressed. Here a lsit of commands:
+
+-ARROW UP:   send a "w" message that launch a .forward() function on the turtle
+-ARROW LEFT: send a "a" message that launch a .left() function on the turtle
+-ARROW RIGHT:send a "d" message that launch a .right() function on the turtle
+-ARROW DOWN: send a "s" message that launch a .backward() function on the turtle
+-ESC:        send a "esc" message that remove the created turtle and stop the client
+"""
+
 import socket
 from pynput.keyboard import Key, Listener
 
@@ -7,7 +19,7 @@ server_port=10000
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def on_press(key):
+def on_press(key):  #send command based on key pressed
     if key == Key.up:
         client.sendto("w".encode(),(server_ip,server_port))
     if key == Key.down:
@@ -24,10 +36,11 @@ def on_release(key):
 
 
 def main():
+    #launching on press and on release listeners
     with Listener(
         on_press=on_press,
         on_release=on_release) as listener:
-            listener.join()
+            listener.join() 
 
 if __name__ == "__main__":
     main()
--- sistemi_IV/python/es017_turtle_server/server.py
+++ sistemi_IV/python/es017_turtle_server/server.py
@@ -1,3 +1,10 @@
+"""
+Author: BRuno Luca
+Date: 16-05-2020"
+This program create a UDP serevr process that receive a message ("w", "a" , "s" , "d"). The message is passed to a dictionary of cammands that compute it and make 
+the turle move.
+"""
+
 import socket
 import turtle
 import random
@@ -5,12 +12,13 @@ import random
 server_ip = '127.0.0.1'
 server_port = 10000
 
-main_turtle = turtle.Turtle()
+main_turtle = turtle.Turtle()   #creating a main turtle and hiding it
 main_turtle.hideturtle()
 
-turtle_table = {}
-connection_table = []
+turtle_table = {}   #a turtle storage costruct {(client_ip, "client_port"): turtle.Turtle(), ...}
+connection_table = []   #a connection storage construct connection_table[0] = (client_ip, "client_port")
 
+#defining command function
 def forward(x,t):
     t.forward(x)
 def backward(x,t):
@@ -20,37 +28,37 @@ def left(x,t):
 def right(x,t):
     t.right(x)
 
-
+#a commands dictionary for computing command received from client
 commands = {
     "w": forward,
     "a": left,
     "s": backward,
     "d": right,
 }
-command_list = [k for k,_ in commands.items()]
+command_list = [k for k,_ in commands.items()]  #set a lsit of available command, to error prevent
     
 
 
 
 def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   #creating UDP server with IPv4 datagrams 
     server.bind((server_ip, server_port))
 
     while True:
-        command, address = server.recvfrom(4096)
+        command, address = server.recvfrom(4096)    #storing command send and address of client
         command = command.decode()[0:]
 
         print(address)
         print(connection_table)
         print(turtle_table)
 
-        if address in connection_table:
-            if command == "esc":
+        if address in connection_table: #check for the sending address in the address table
+            if command == "esc":    #delete the turtle
                 turtle_table.pop(address)
                 connection_table.pop(connection_table.index(address))
-            elif command in command_list:
+            elif command in command_list:   #check if command received is in command table
                 commands[command](10,turtle_table[address])
-        else:
+        else:   #creating new turtle and adding it to the turtle table
             connection_table.append(address)
             turtle_table[address] = main_turtle.clone()
             turtle_table[address].showturtle()
