9a880981865d7e8a1593c4e27b1b5c0de790f067
Luca Bruno <luca.bru@protonmail.com>
2020-09-14 20:22:32 +0200

    Summer Homeworks

---
+++ tpsit_IV/summer_works/es11_5.py
@@ -0,0 +1,24 @@
+"""
+Author: Bruno Luca
+Date: 14-09-2020
+Title: rotate word
+"""
+
+def load_dictionary(file_name):
+    d = dict()
+    fin = open(file_name)
+    for i,line in enumerate(fin):
+        d[line.strip()] = i
+
+    fin.close()
+
+    return d
+
+def are_rotable(w1,w2):
+    
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
