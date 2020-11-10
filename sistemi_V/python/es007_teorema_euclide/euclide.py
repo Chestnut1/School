313c29ff6cdd81ddc13196264c750870266de1a0
Luca Bruno <luca.bru@protonmail.com>
2020-11-10 10:29:25 +0100

    euclide

---
+++ sistemi_V/python/es007_teorema_euclide/euclide.py
@@ -0,0 +1,28 @@
+"""
+Author: Bruno Luca
+Date: 10-11-2020
+Title: euclide theoreme
+"""
+
+def euclide(a,b):
+    if b > a:
+        c = a
+        a = b
+        b = c
+
+    while True:
+        newa = a % b
+        if newa == 0:
+            return b
+            break
+        else:
+            a = b
+            b = newa
+
+def main(a = int(input("A>> ")), b = int(input("B>> "))):
+    
+    print(euclide(a,b))
+    
+
+if __name__ == "__main__":
+    main()
