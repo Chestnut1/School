e0a274838e6a6867582a1e2dc4204279829de10a
Luca Bruno <luca.bru@protonmail.com>
2020-11-16 16:47:37 +0100

    .

--- informatica_V/java/es003_statement/src/TestStatement.java
+++ informatica_V/java/es003_statement/src/TestStatement.java
@@ -1,38 +1,33 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class TestStatement {
     public static void main(String args[]) {
-        String URL = "jdbc:sqlite:test.db";
-        String driver = "org.sqlite.JDBC";
-        int iResult = 0;
 
-        Connection con = null;
-        ResultSet result;
+        Scanner input = new Scanner(System.in);
 
-        try {
-            Class.forName(driver);
-            con = DriverManager.getConnection(URL);
-            System.out.println("Connessione fatta.");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("Driver non trovato");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Errore di connessione.");
-        }
-        if (con != null) {
-            try {
-                Statement stat1 = con.createStatement();
-                result = stat1.executeQuery("SELECT * FROM impiegati;");
-                Statement stat2 = con.createStatement();
-                iResult = stat2.executeUpdate("INSERT INTO impiegati (nome, cognome, citta) VALUES ('Filo','Sganga','Paperopoli');");
-                System.out.println("Record aggiunti: " + iResult);
-                con.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-                System.out.println("Errore di esecuzione query.");
-            }
-        }
+        DB myDB = new DB();
 
+        System.out.println("Record modificato: " + myDB.executeUpdate("UPDATE SedeA SET CodR = 3 WHERE CodOperaio = 'PLFR'"));
+
+        System.out.println("Record modificato: " + myDB.executeUpdate("DELETE FROM SedeB WHERE CodOperaio = 'RSMR'"));
+
+
+        int resp1 = Integer.parseInt(String.valueOf(input.next()));
+        int resp2 = Integer.parseInt(String.valueOf(input.next()));
+
+        System.out.println("Record modificato: " + myDB.executeUpdate("UPDATE SedeA SET Stipendio = Stipendio+30 WHERE CodR=  '" + resp1 + "' OR CodR= '" + resp2 + "'"));
+
+        System.out.println("Record modificato: " + myDB.executeUpdate("INSERT INTO SedeB VALUES ('RSMR','F','2011-05-05',880.78,2)"));
+
+        String nome = String.valueOf(input.next());
+        String cognome = String.valueOf(input.next());
+
+        System.out.println("Selezionato: \n" + myDB.executeQuery("SELECT CodOperaio, Assunto_il FROM SedeB, Responsabili WHERE CodR = CodRep AND Nome = '" + nome + "' AND Cognome = '" + cognome + "' ORDER BY Assunto_il"));
+
+        System.out.println("MAX: " + myDB.executeQuery("SELECT MAX(Stipendio) FROM SedeA") + "\n" + "AVG: " + myDB.executeQuery("SELECT AVG(Stipendio) FROM SedeA") + "\n" + "MIN: " + myDB.executeQuery("SELECT MIN(Stipendio) FROM SedeA") + "\n" );
+
+        resp1 = Integer.parseInt(String.valueOf(input.next()));
+        System.out.println("COUNT: " + myDB.executeQuery("SELECT COUNT(*) FROM SedeB WHERE CodR = " + resp1));
     }
 }
