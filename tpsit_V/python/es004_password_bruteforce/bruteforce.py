1b9b295c3b7196bfea4c3fb57b438df26e212ead
Luca Bruno <luca.bruno@itiscuneo.eu>
2021-02-04 07:41:54 +0100

    .

--- tpsit_V/python/es004_password_bruteforce/bruteforce.py
+++ tpsit_V/python/es004_password_bruteforce/bruteforce.py
@@ -16,24 +16,20 @@ def client():
     c.connect((server_ip,server_port))
 
     while True:
-        """
-        POST /auth HTTP/1.1\r\n
-        Content-Type: application/x-www-form-urlencoded\r\n
-        Content-Length: 29\r\n
-        Host: localhost:9000\r\n
-        Connection: close\r\n
-        \r\n
-        userName=Ganesh&password=pass
-        """
+        
 
-        body = "usr=luca@mail.com&pwd=4"
+        body = "usr=luca@mail.com&pwd=1234"
         msg = f"POST http://127.0.0.1:5000 HTTP/1.1\r\nHost: localhost:5000\r\nConnection: open\r\nContent_Type= 'application/x-www-form-urlencoded'\nContent_Length= {len(body)}\r\n\r\n"+body
         print(f"sending:\n\n{msg}\n\n")
         c.sendall(msg.encode())
-        echo_msg = c.recv(4096)
-        print(f"ECHO>> {echo_msg}")
-        if len(echo_msg) > 0:
-            break
+
+        
+        while True:
+            echo_msg = c.recv(4096)
+
+            print(f"ECHO>> {echo_msg}")
+            if len(echo_msg) <= 0:
+                break
     
     c.close()
 
--- tpsit_V/python/es004_password_bruteforce/main.py
+++ tpsit_V/python/es004_password_bruteforce/main.py
@@ -41,4 +41,4 @@ def validate(username,password):
         return False
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", debug = False)
+    app.run(host="127.0.0.1", debug = True)
