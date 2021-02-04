dfcc89cfda9e5104be86533ded6d1b665b86aac5
Luca Bruno <luca.bru@protonmail.com>
2021-02-04 19:22:08 +0100

    .

--- tpsit_V/python/es004_password_bruteforce/main.py
+++ tpsit_V/python/es004_password_bruteforce/main.py
@@ -13,50 +13,72 @@ import sqlite3
 app = Flask(__name__)
 
 @app.route("/")             # http://127.0.0.1:5000/
-def index():                # decoratore. Per accedere alla pagina associata alla funzione index 
+def index():                
     return render_template("login.html")
 
 
 @app.route("/", methods = ["GET","POST"])
 def login():
+
+    """
+    This function is called when the login button is pressed.
+    It takes username and the password that is instantly hashed in SHA512.
+    Connect to the credential DB and check for corrispondace and let the user see a secret page.
+    """
+
     if request.method == "POST":
+        #get username and password
         usr = request.form["usr"]
         pwd = hashlib.sha512(request.form["pwd"].encode()).hexdigest()
 
-        completion = validate(usr,pwd)
+        completion = validate(usr,pwd)  #check if password is correct
 
         if completion:
-            return redirect(url_for("secret"))
+            return redirect(url_for("secret"))  #redirect user to the secret page
         else:
-            error  = "Invalid Credentials. Please try again."
+            error  = "Invalid Credentials. Please try again."   #stay in the login page and say wrong credentials
         
         return render_template("login.html", error = error)
 
 def validate(username,password):
-    with sqlite3.connect("static/credential.db") as conn:
-        cursor = conn.cursor()
 
+    """
+    This function is called when the login button is pressed.
+    It takes username and the hashed password. It connect to the credential DB and check if 
+    username and password are correct.
+    """
+
+    with sqlite3.connect("static/credential.db") as conn:   #connect to DB
+        cursor = conn.cursor()  #create a cursor
+
+        #search for user email
         cursor.execute(f"SELECT email FROM users WHERE email = '{username}' AND password = '{password}' ")
         corrispondance = cursor.fetchall()
 
-    print(len(corrispondance))
-
-    return len(corrispondance)
+    return len(corrispondance) # 1 --> user found!  |  0 --> user not found!
 
 @app.route("/signup", methods = ["POST"])
 def signup():
+
+    """
+    When Signup button is pressed load this page. It allow user to insert a new email and password 
+    in the credential DB.
+    If user already exist say it to the user and stay in the signup page.
+    """
+
+    #get username and password
     usr = request.form["usr"]
     pwd = hashlib.sha512(request.form["pwd"].encode()).hexdigest()
 
-    print(pwd)
-
+    #connect to db
     with sqlite3.connect("static/credential.db") as conn:
-        cursor = conn.cursor()
+        cursor = conn.cursor()  #create a cursor 
 
+        #selecet all user with the same email to see if it already exist
         cursor.execute(f"SELECT email FROM users WHERE email = '{usr}' ")
         corrispondance = cursor.fetchall()
 
-    if len(corrispondance) == 0:
+    if len(corrispondance) == 0:    #user dows not exist
         with sqlite3.connect("static/credential.db") as conn:
             cursor = conn.cursor()
 
@@ -64,18 +86,24 @@ def signup():
             corrispondance = cursor.fetchall()
 
         return "User created!"
-    else:
+    else:   #user already exist
         error  = "User already exist!"
         
     return render_template("signup", error = error)
 
     
 @app.route("/secret")
-def secret():
+def secret():   
+    """
+    This function rendere a secret page.
+    """
     return "This is a scerte page."
 
 @app.route("/signup")
 def load_signup():
+    """
+    This function rendere a page where user can add email and password to credentail DB.
+    """
     return render_template("signup.html")
 
 
